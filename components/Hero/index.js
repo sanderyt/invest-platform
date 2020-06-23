cf5edd00bc493e9d0a19112d7ab80c363a4701cb
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-06-23 11:54:37 +0200

    hero component for landing page

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,18 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className="hero container-fluid">
-      <div className="row d-flex flex-column">
+      <div className="row">
         <div className="col-12 d-flex flex-column align-items-center justify-content-center">
           <div className="hero__box d-flex flex-column align-items-center justify-content-center">
             <h1 className="mt-5 text--white">
               Het crowdfunding platform voor het kopen van websites
             </h1>
             <h2 className="text--white">
-              Genereer samen met mij een passief inkomen met "digitaal" vastgoed
+              Genereer een stabiel passief inkomen met "online" vastgoed
             </h2>
-            <button className="btn btn--lg">Bekijk aangeboden websites</button>
+            <Link href="/aanbod">
+              <a>
+                <button className="btn btn--cta btn--lg">
+                  Bekijk aangeboden websites
+                </button>
+              </a>
+            </Link>
           </div>
         </div>
       </div>
