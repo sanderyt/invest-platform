39bc79cf1bfdd9a136106e0ab8d3582fc1908ee4
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-06-23 21:42:49 +0200

    responsive fixes for website card

--- components/WebsiteCard/index.js
+++ components/WebsiteCard/index.js
@@ -6,7 +6,7 @@ import ProgressBar from "../ProgressBar";
 const WebsiteCard = () => {
   return (
     <Link href="/aanbod/1234">
-      <div className="col-sm-6 col-md-6 col-lg-4">
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
         <div className="card m-4">
           <img src="/thumb.png" className="card__thumb" />
           <div className="card__info p-3 d-flex flex-column justify-content-center align-items-center">
