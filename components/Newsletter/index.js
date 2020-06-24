22caf78eac2b6303483fc4e1ee713b96d8e15b95
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-06-24 11:39:49 +0200

    added newsletter to homepage

--- components/Newsletter/index.js
+++ components/Newsletter/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+import InputField from "../InputField";
+import Button from "../Button";
+
 const Newsletter = () => {
   return (
-    <div>
-      <h1>Mis nooit meer een website. Abboneer op onze alert!</h1>
+    <div className="container-fluid m-5">
+      <div className="newsletter row d-flex justify-content-center">
+        <div className="newsletter__overlay" />
+        <div className="newsletter__content d-flex flex-column justify-content-center align-items-center">
+          <h1>Mis nooit meer een website. Abboneer op onze alert.</h1>
+          <h2>Vul hieronder je e-mailadres in en mis nooit meer een kans.</h2>
+          <div className="d-flex align-items-center">
+            <InputField />
+            <button className="btn btn--cta btn--lg">Aanmelden</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Latest from "../components/Latest";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
     <Layout>
       <Hero />
       <Latest />
+      <Newsletter />
     </Layout>
   );
 };
