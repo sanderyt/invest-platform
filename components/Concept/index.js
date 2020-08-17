e20b757a20dab48f282dc77f242a0df57f6310b8
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-08-17 19:39:51 +0200

    some content on the homepage

--- components/Concept/index.js
+++ components/Concept/index.js
@@ -4,19 +4,24 @@ const Comcept = () => {
   return (
     <div className="container-fluid">
       <div className="row py-5 bg--grey100">
-        <div className="col-sm-6 d-flex flex-column align-items-center">
+        <div className="col-sm-6 d-flex flex-column align-items-center p-5">
           <h2>
-            Investeer in <span className="text--primary">online</span> vastgoed
+            Investeer in <span className="text--primary">winstgevende</span>{" "}
+            websites 💰💰💰
           </h2>
           <p>
-            Samen investeren in digitale objecten en passief inkomen genereren.
-            Met mijn jarenlange expertise in affiliate marketing, weet ik
-            precies hoe ik een website draaiende moet houden.
+            De laatste jaren worden er steeds meer websites en webshops verkocht
+            via verschillende platformen. Meestal hebben deze websites al zich
+            bewezen in het verleden en draaien vaak al inkomsten. Een gemiddelde
+            website die wordt aangeboden via een dergelijk platform, wordt vaak
+            binnen 2-3 jaar terugverdiend. In Nederland is het grootste platform
+            webshopovername.nl. In samenwerking met hen, bieden wij de beste
+            affiliate websites aan.
           </p>
           <p>
             Samen investeren in digitale objecten en passief inkomen genereren.
             Met mijn jarenlange expertise in affiliate marketing, weet ik
-            precies hoe ik een website draaiende moet houden. 🚀 
+            precies hoe ik een website draaiende moet houden.
           </p>
         </div>
         <div className="col-sm-6 d-flex flex-column align-items-center">
@@ -25,7 +30,10 @@ const Comcept = () => {
       </div>
       <div className="row my-5">
         <div className="col-sm-6 d-flex flex-column align-items-center">
-          <h2>Bouw een duurzaam passief inkomen op</h2>
+          <img src="/newyork.jpg" className="image" />
+        </div>
+        <div className="col-sm-6 d-flex flex-column align-items-center p-5">
+          <h2>Wat is een affiliate website? 😍 </h2>
           <p>
             Samen investeren in digitale objecten en passief inkomen genereren.
             Met mijn jarenlange expertise in affiliate marketing, weet ik
@@ -37,8 +45,45 @@ const Comcept = () => {
             precies hoe ik een website draaiende moet houden.
           </p>
         </div>
+      </div>
+      <div className="row py-5 bg--grey100">
+        <div className="col-sm-6 d-flex flex-column align-items-center p-5">
+          <h2>
+            Met onze expertise, blijven de inkomsten stabiel of groeien 🚀
+          </h2>
+          <p>
+            Doordat wij al jaren ervaring hebben met het onderhouden van dit
+            soort websites, blijven de websites jarenlang stabiel draaien.
+            Dergelijke kennis heb jij zelf niet nodig. We vinden het natuurlijk
+            wel enorm leuk om het uit te leggen, mocht je daar interesse in
+            hebben.
+          </p>
+          <p>
+            Samen investeren in digitale objecten en passief inkomen genereren.
+            Met mijn jarenlange expertise in affiliate marketing, weet ik
+            precies hoe ik een website draaiende moet houden.
+          </p>
+        </div>
+        <div className="col-sm-6 d-flex flex-column align-items-center">
+          <img src="/ubud.jpg" className="image" />
+        </div>
+      </div>
+      <div className="row my-5">
         <div className="col-sm-6 d-flex flex-column align-items-center">
           <img src="/newyork.jpg" className="image" />
+        </div>
+        <div className="col-sm-6 d-flex flex-column align-items-center">
+          <h2>Wat is een affiliate website? 🤫 </h2>
+          <p>
+            Samen investeren in digitale objecten en passief inkomen genereren.
+            Met mijn jarenlange expertise in affiliate marketing, weet ik
+            precies hoe ik een website draaiende moet houden.
+          </p>
+          <p>
+            Samen investeren in digitale objecten en passief inkomen genereren.
+            Met mijn jarenlange expertise in affiliate marketing, weet ik
+            precies hoe ik een website draaiende moet houden.
+          </p>
         </div>
       </div>
     </div>
